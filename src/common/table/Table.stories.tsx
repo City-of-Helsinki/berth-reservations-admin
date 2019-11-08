383fec56b0137e49996f7ba84c303a2d91617bdd
Timo Aho <zeukkari@gmail.com>
2019-11-08 14:44:04 +0200

    Revert "fix linting"
    This reverts commit 5f87bea32f4eb0dfb0d9ac1f14ce5585163e4aeb.

--- src/common/table/Table.stories.tsx
+++ src/common/table/Table.stories.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import React from 'react';
 
 import Table from './Table';
@@ -8,32 +7,34 @@ export default {
   title: 'Table',
 };
 
-const MockData = [
-  { a: 'foobar', b: 'foobar', c: 'foobar' },
-  { a: 'foobar', b: 'foobar', c: 'foobar' },
-  { a: 'foobar', b: 'foobar', c: 'foobar' },
-  { a: 'foobar', b: 'foobar', c: 'foobar' },
-  { a: 'foobar', b: 'foobar', c: 'foobar' },
+const MOUNTAINS = [
+  { name: 'Kilimanjaro', height: 5895, country: 'Tanzania' },
+  { name: 'Everest', height: 8848, country: 'Nepal' },
+  { name: 'Mount Fuji', height: 3776, country: 'Japan' },
+  { name: 'Mont Blanc', height: 4808, country: 'Italy/France' },
+  { name: 'Vaalserberg', height: 323, country: 'Netherlands' },
+  { name: 'Denali', height: 6168, country: 'United States' },
+  { name: 'Popocatepetl', height: 5465, country: 'Mexico' },
 ];
 
 const columns: any[] = [
   {
     Header: 'name',
-    accessor: 'a',
+    accessor: 'name',
   },
   {
     Header: 'height',
-    accessor: 'b',
+    accessor: 'height',
   },
   {
     Header: 'country',
-    accessor: 'c',
+    accessor: 'country',
   },
 ];
 
 export const Simple = () => (
   <Table
-    data={MockData}
+    data={MOUNTAINS}
     columns={columns}
     renderSubComponent={row => row.index}
     renderMainHeader={() => 'Demos'}
