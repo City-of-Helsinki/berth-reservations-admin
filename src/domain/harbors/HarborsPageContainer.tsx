83dbb8380fa54e8011bf6844ca79bef2a926b3c4
Nizar Rahme <nizar.w.rahmah@gmail.com>
2019-11-18 11:44:24 +0200

    Implement HarborsPage

--- src/domain/harbors/HarborsPageContainer.tsx
+++ src/domain/harbors/HarborsPageContainer.tsx
@@ -9,12 +9,10 @@ import { HARBORS_QUERY } from './harborsQuery';
 import Table from '../../common/table/Table';
 import { getHarborsData, HarborData } from './utils';
 import { HARBORS } from './__generated__/HARBORS';
-import Card from '../../common/card/Card';
-import Text from '../../common/text/Text';
-import Paragraph from '../../common/paragraph/Paragraph';
-import LabelValuePair from '../../common/labelValuePair/LabelValuePair';
-import Box from '../../common/box/Box';
 import Icon from '../../common/icon/Icon';
+import HarborDetails from './harborDetails/HarborDetails';
+import Page from '../page/Page';
+import HarborsPage from './HarborsPage';
 
 interface Props2 {
   name?: string;
@@ -30,76 +28,76 @@ interface Props {
   data: Props2;
 }
 
-export const HarborDetails = ({ data }: Props) => {
-  const address = `${data.streetAddress} ${data.zipCode} ${data.municipality}`;
-  const servicemapUrl = `http://palvelukartta.hel.fi/unit/${data.servicemapId}`;
+// export const HarborDetails = ({ data }: Props) => {
+//   const address = `${data.streetAddress} ${data.zipCode} ${data.municipality}`;
+//   const servicemapUrl = `http://palvelukartta.hel.fi/unit/${data.servicemapId}`;
 
-  const Harbor1 = () => (
-    <Card>
-      <Paragraph>
-        <figure>
-          <img src={data.imageFile} width="50%" alt={data.name}></img>
-          <figcaption>{data.name}</figcaption>
-        </figure>
-      </Paragraph>
-      <Paragraph title="Osoite">
-        <Text color="brand" size="xs">
-          {address}
-        </Text>
-      </Paragraph>
-      <Paragraph>
-        <Text color="brand" size="s">
-          <a href={data.wwwUrl}>Toimipisteen nettisivut</a>
-        </Text>
-      </Paragraph>
-      <Paragraph>
-        <Text color="brand" size="s">
-          Satamakartta (PDF)
-        </Text>
-      </Paragraph>
-      <Paragraph>
-        <Text color="brand" size="s">
-          <a href={servicemapUrl}>Palvelukartta</a>
-        </Text>
-      </Paragraph>
-    </Card>
-  );
+//   const Harbor1 = () => (
+//     <Card>
+//       <Paragraph>
+//         <figure>
+//           <img src={data.imageFile} alt={data.name}></img>
+//           <figcaption>{data.name}</figcaption>
+//         </figure>
+//       </Paragraph>
+//       <Paragraph title="Osoite">
+//         <Text color="brand" size="xs">
+//           {address}
+//         </Text>
+//       </Paragraph>
+//       <Paragraph>
+//         <Text color="brand" size="s">
+//           <a href={data.wwwUrl}>Toimipisteen nettisivut</a>
+//         </Text>
+//       </Paragraph>
+//       <Paragraph>
+//         <Text color="brand" size="s">
+//           Satamakartta (PDF)
+//         </Text>
+//       </Paragraph>
+//       <Paragraph>
+//         <Text color="brand" size="s">
+//           <a href={servicemapUrl}>Palvelukartta</a>
+//         </Text>
+//       </Paragraph>
+//     </Card>
+//   );
 
-  const Harbor2 = () => (
-    <Card>
-      <Paragraph>
-        <LabelValuePair label="Max leveys" value="2.5m - 4m" />
-        <LabelValuePair
-          label="Kiinnitys"
-          value="Aisa-, Kävelyaisa- ja Peräpoijupaikkoja"
-        />
-        <LabelValuePair
-          label="Päällikkö"
-          value="Mikko Mallikas +358 00 000 000"
-        />
-        <LabelValuePair label="Huoltotiimi" value="Itäinen veneilytiimi" />
-      </Paragraph>
-    </Card>
-  );
+//   const Harbor2 = () => (
+//     <Card>
+//       <Paragraph>
+//         <LabelValuePair label="Max leveys" value="2.5m - 4m" />
+//         <LabelValuePair
+//           label="Kiinnitys"
+//           value="Aisa-, Kävelyaisa- ja Peräpoijupaikkoja"
+//         />
+//         <LabelValuePair
+//           label="Päällikkö"
+//           value="Mikko Mallikas +358 00 000 000"
+//         />
+//         <LabelValuePair label="Huoltotiimi" value="Itäinen veneilytiimi" />
+//       </Paragraph>
+//     </Card>
+//   );
 
-  const Harbor3 = () => (
-    <Card>
-      <Paragraph title="Viimeaikainen toiminta">
-        <Text color="brand" size="xs">
-          Ei mitään
-        </Text>
-      </Paragraph>
-    </Card>
-  );
+//   const Harbor3 = () => (
+//     <Card>
+//       <Paragraph title="Viimeaikainen toiminta">
+//         <Text color="brand" size="xs">
+//           Ei mitään
+//         </Text>
+//       </Paragraph>
+//     </Card>
+//   );
 
-  return (
-    <Box>
-      <Harbor1 />
-      <Harbor2 />
-      <Harbor3 />
-    </Box>
-  );
-};
+//   return (
+//     <Grid colsCount={3}>
+//       <Harbor1 />
+//       <Harbor2 />
+//       <Harbor3 />
+//     </Grid>
+//   );
+// };
 
 type ColumnType = Column<HarborData> & { accessor: keyof HarborData };
 
@@ -178,13 +176,17 @@ const HarborsContainer: React.FC = () => {
   const tableData = getHarborsData(data);
 
   return (
-    <Table
-      data={tableData}
-      columns={columns}
-      renderSubComponent={row => <HarborDetails data={row.original} />}
-      renderMainHeader={() => t('harbors.tableHeaders.mainHeader')}
-      canSelectRows
-    />
+    <Page>
+      <HarborsPage>
+        <Table
+          data={tableData}
+          columns={columns}
+          renderSubComponent={row => <HarborDetails {...row.original} />}
+          renderMainHeader={() => t('harbors.tableHeaders.mainHeader')}
+          canSelectRows
+        />
+      </HarborsPage>
+    </Page>
   );
 };
 
