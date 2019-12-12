f03feae1619877fdb5c528a987c16e6aec779d0c
Timo Aho <zeukkari@gmail.com>
2019-12-12 11:28:29 +0200

    Tinkering

--- src/domain/auth/route/PrivateRoute.tsx
+++ src/domain/auth/route/PrivateRoute.tsx
@@ -24,7 +24,7 @@ const PrivateRoute: FunctionComponent<PrivateRouteProps> = ({
         ) : (
           <Redirect
             to={{
-              pathname: `/home`,
+              pathname: `/`,
               state: { from: location },
             }}
           />
