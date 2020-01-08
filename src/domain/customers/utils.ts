42403b34f685a4955e47f199a17e4c9bda11facc
Nizar Rahme <nizar.w.rahmah@gmail.com>
2020-01-08 11:23:09 +0200

    Refactor getCustomersData utility function

--- src/domain/customers/utils.ts
+++ src/domain/customers/utils.ts
@@ -2,29 +2,39 @@ import { CUSTOMERS } from './__generated__/CUSTOMERS';
 
 export interface CustomerData {
   id: string;
-  firstName: string;
-  lastName: string;
-  nickname: string | null;
-  email: string | false | null;
-  phone: string | false | null;
+  name: string;
+  nickname: string;
+  // email: string | false | null;
+  // phone: string | false | null;
 }
 
-export const getCustomersData = (data: CUSTOMERS | undefined | any) => {
-  if (data?.profiles?.edges) {
-    return data.profiles.edges.map(profile => {
-      if (profile?.node) {
-        const profileData: CustomerData = {
-          email: profile.node.emails[0]?.email,
-          firstName: profile.node.firstName,
-          id: profile.node.id,
-          lastName: profile.node.lastName,
-          nickname: profile.node.nickname,
-          phone: profile.node.phones[0]?.phone,
-        };
-        return profileData;
-      }
-      return [];
-    });
-  }
-  return [];
+export const getCustomersData = (data: CUSTOMERS | undefined) => {
+  return data?.profiles?.edges.reduce<CustomerData[]>((acc, profile) => {
+    if (profile?.node) {
+      const profileData = {
+        // email: profile.node.emails?.[0]?.email,
+        name: `${profile.node.firstName} ${profile.node.lastName}`,
+        id: profile.node.id,
+        nickname: profile.node.nickname || '',
+        // phone: profile.node.phones?.[0]?.phone,
+      };
+      return [...acc, profileData];
+    }
+    return acc;
+  }, []);
+
+  // return data?.profiles?.edges.map(profile => {
+  //   if (profile?.node) {
+  //     const profileData: CustomerData = {
+  //       // email: profile.node.emails?.[0]?.email,
+  //       firstName: profile.node.firstName,
+  //       id: profile.node.id,
+  //       lastName: profile.node.lastName,
+  //       nickname: profile.node.nickname,
+  //       // phone: profile.node.phones?.[0]?.phone,
+  //     };
+  //     return profileData;
+  //   }
+  //   return [];
+  // });
 };
