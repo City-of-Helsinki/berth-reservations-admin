b371c38d8fcd4216ece9652c06ba0b69fc40f42f
Nizar Rahme <nizar.w.rahmah@gmail.com>
2020-01-20 16:04:19 +0200

    Remove the redundant key prop

--- src/common/labelValuePair/LabelValuePair.tsx
+++ src/common/labelValuePair/LabelValuePair.tsx
@@ -5,7 +5,7 @@ import styles from './labelValuePair.module.scss';
 
 interface Props {
   label: string;
-  value?: string | null | React.ReactElement;
+  value?: string | null;
   labelColor?: 'standard' | 'brand' | 'critical' | 'secondary';
   align?: 'left' | 'center' | 'right';
 }
@@ -15,17 +15,13 @@ const LabelValuePair = ({
   value,
   labelColor = 'standard',
   align = 'left',
-}: Props) => {
-  return (
-    <div className={styles.labelValuePair}>
-      <span className={classNames(styles.label, styles[labelColor])}>
-        {label}:
-      </span>
-      <span key="value" className={classNames(styles.value, styles[align])}>
-        {value}
-      </span>
-    </div>
-  );
-};
+}: Props) => (
+  <div className={styles.labelValuePair}>
+    <span className={classNames(styles.label, styles[labelColor])}>
+      {label}:
+    </span>
+    <span className={classNames(styles.value, styles[align])}>{value}</span>
+  </div>
+);
 
 export default LabelValuePair;
