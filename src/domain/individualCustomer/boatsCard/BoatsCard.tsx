32757233361a0dacc0fd98a3c12d302d93f34f0e
Timo Aho <zeukkari@gmail.com>
2020-01-27 12:26:17 +0200

    split boatscard into boatcard and boatscard

--- src/domain/individualCustomer/boatsCard/BoatsCard.tsx
+++ src/domain/individualCustomer/boatsCard/BoatsCard.tsx
@@ -30,86 +30,94 @@ export interface BoatsCardProps {
   boats: (Boat | LargeBoat)[];
 }
 
-const BoatsCard: React.SFC<BoatsCardProps> = ({ boats }) => {
+export interface BoatCardProps {
+  boat: Boat | LargeBoat;
+}
+
+const BoatCard: React.SFC<BoatCardProps> = ({ boat }) => {
   const { t } = useTranslation();
   const isLargeBoat = (boat: LargeBoat | Boat): boat is LargeBoat =>
     (boat as LargeBoat).boatPower !== undefined;
 
   return (
+    <Card
+      className={styles.boatsCard}
+      key={boat.id}
+      title={t('individualCustomer.customerBoats.title')}
+    >
+      <Paragraph>
+        <LabelValuePair
+          label={t('individualCustomer.customerBoats.boatWidth')}
+          value={boat.boatType}
+        />
+        <LabelValuePair
+          label={t('individualCustomer.customerBoats.registrationNumber')}
+          value={boat.registrationNumber}
+        />
+      </Paragraph>
+      <Paragraph>
+        <LabelValuePair
+          label={t('individualCustomer.customerBoats.boatLength')}
+          value={boat.boatLength}
+        />
+        <LabelValuePair
+          label={t('individualCustomer.customerBoats.boatDepth')}
+          value={boat.boatDepth}
+        />
+        <LabelValuePair
+          label={t('individualCustomer.customerBoats.boatWeight')}
+          value={boat.boatWeight}
+        />
+      </Paragraph>
+      <Paragraph>
+        <LabelValuePair
+          label={t('individualCustomer.customerBoats.boatName')}
+          value={boat.boatName}
+        />
+        <LabelValuePair
+          label={t('individualCustomer.customerBoats.boatBrand')}
+          value={boat.boatBrand}
+        />
+      </Paragraph>
+      {isLargeBoat(boat) && (
+        <>
+          <Paragraph>
+            <LabelValuePair
+              label={t('individualCustomer.customerBoats.boatPower')}
+              value={boat.boatPower}
+            />
+            <LabelValuePair
+              label={t('individualCustomer.customerBoats.boatMaterial')}
+              value={boat.boatMaterial}
+            />
+            <LabelValuePair
+              label={t('individualCustomer.customerBoats.purpose')}
+              value={boat.purpose}
+            />
+          </Paragraph>
+          <Paragraph>
+            <LabelValuePair
+              label={t('individualCustomer.customerBoats.inspection')}
+              value={boat.inspection}
+            />
+          </Paragraph>
+          <Paragraph>
+            <LabelValuePair
+              label={t('individualCustomer.customerBoats.insurance')}
+              value={boat.insurance}
+            />
+          </Paragraph>
+        </>
+      )}
+    </Card>
+  );
+};
+
+const BoatsCard: React.SFC<BoatsCardProps> = ({ boats }) => {
+  return (
     <div>
       {boats.map((boat, i) => (
-        <Card
-          className={styles.boatsCard}
-          key={boat.id}
-          title={
-            i === 0 ? t('individualCustomer.customerBoats.title') : undefined
-          }
-        >
-          <Paragraph>
-            <LabelValuePair
-              label={t('individualCustomer.customerBoats.boatWidth')}
-              value={boat.boatType}
-            />
-            <LabelValuePair
-              label={t('individualCustomer.customerBoats.registrationNumber')}
-              value={boat.registrationNumber}
-            />
-          </Paragraph>
-          <Paragraph>
-            <LabelValuePair
-              label={t('individualCustomer.customerBoats.boatLength')}
-              value={boat.boatLength}
-            />
-            <LabelValuePair
-              label={t('individualCustomer.customerBoats.boatDepth')}
-              value={boat.boatDepth}
-            />
-            <LabelValuePair
-              label={t('individualCustomer.customerBoats.boatWeight')}
-              value={boat.boatWeight}
-            />
-          </Paragraph>
-          <Paragraph>
-            <LabelValuePair
-              label={t('individualCustomer.customerBoats.boatName')}
-              value={boat.boatName}
-            />
-            <LabelValuePair
-              label={t('individualCustomer.customerBoats.boatBrand')}
-              value={boat.boatBrand}
-            />
-          </Paragraph>
-          {isLargeBoat(boat) && (
-            <>
-              <Paragraph>
-                <LabelValuePair
-                  label={t('individualCustomer.customerBoats.boatPower')}
-                  value={boat.boatPower}
-                />
-                <LabelValuePair
-                  label={t('individualCustomer.customerBoats.boatMaterial')}
-                  value={boat.boatMaterial}
-                />
-                <LabelValuePair
-                  label={t('individualCustomer.customerBoats.purpose')}
-                  value={boat.purpose}
-                />
-              </Paragraph>
-              <Paragraph>
-                <LabelValuePair
-                  label={t('individualCustomer.customerBoats.inspection')}
-                  value={boat.inspection}
-                />
-              </Paragraph>
-              <Paragraph>
-                <LabelValuePair
-                  label={t('individualCustomer.customerBoats.insurance')}
-                  value={boat.insurance}
-                />
-              </Paragraph>
-            </>
-          )}
-        </Card>
+        <BoatCard boat={boat} />
       ))}
     </div>
   );
