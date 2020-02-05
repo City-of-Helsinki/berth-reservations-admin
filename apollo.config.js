9184cb95f1bba24ea5edaece6adb767f17b2f311
Timo Aho <zeukkari@gmail.com>
2020-02-05 21:31:17 +0200

    use local schema file in apollo config

--- apollo.config.js
+++ apollo.config.js
@@ -1,11 +1,8 @@
-// eslint-disable-next-line
-require('dotenv').config({ path: '.env.development.local' });
-
 module.exports = {
   client: {
     service: {
       name: 'Venepaikka Admin Interface',
-      url: process.env.REACT_APP_API_URI,
+      localSchemaFile: './schema.json',
     },
   },
 };
