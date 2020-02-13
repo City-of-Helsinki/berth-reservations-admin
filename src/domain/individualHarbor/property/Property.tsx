198afa7b73438210af70d50e50a381dafc585fdd
Timo Aho <zeukkari@gmail.com>
2020-02-13 06:35:03 +0200

    add outline to property icons

--- src/domain/individualHarbor/property/Property.tsx
+++ src/domain/individualHarbor/property/Property.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
+import classNames from 'classnames';
 
 import Text from '../../../common/text/Text';
 import styles from './property.module.scss';
 
+export interface IconProps {
+  disabled?: boolean;
+  outlined?: boolean;
+  width?: string;
+  height?: string;
+  size?: 'small' | 'standard' | 'large';
+  color?: 'standard' | 'brand' | 'critical' | 'secondary' | 'info';
+}
+
+const IconOutline: React.SFC<IconProps> = ({ children, disabled }) => {
+  return (
+    <div
+      className={classNames(styles.icon, styles.outlined, {
+        [styles.disabled]: disabled,
+      })}
+    >
+      {children}
+    </div>
+  );
+};
+
 interface PropertyProps {
   active?: boolean;
-  Icon?: React.ReactNode;
+  Icon?: Function;
   counter?: number;
   label: string;
 }
@@ -19,12 +41,19 @@ const Property: React.SFC<PropertyProps> = ({
   return (
     <div className={styles.property}>
       {Icon ? (
-        Icon
+        <IconOutline>
+          <Icon
+            className={classNames(styles.icon, styles.outline, {
+              [styles.disabled]: false,
+            })}
+          />
+        </IconOutline>
       ) : (
-        <Text as="h3" size="xxxl" color={active ? 'standard' : 'secondary'}>
-          {counter}
-        </Text>
-      )}
+          <Text as="h3" size="xxxl" color={active ? 'standard' : 'secondary'}>
+            {counter}
+          </Text>
+        )}
+
       <Text as="strong" size="s" color={active ? 'standard' : 'secondary'}>
         {label}
       </Text>
