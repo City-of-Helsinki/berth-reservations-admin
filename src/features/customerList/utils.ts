3407763801ddba8d73ead6c6197e938e7607efb6
Tatu Arvela <tatu.arvela@nitor.com>
2020-10-02 15:51:31 +0300

    Show organization's address information in customer list

--- src/features/customerList/utils.ts
+++ src/features/customerList/utils.ts
@@ -1,40 +1,6 @@
 import { CUSTOMERS, CUSTOMERS_profiles_edges_node as ProfileNode } from './__generated__/CUSTOMERS';
 import { CustomerData, CustomerListApplication, CustomerListBerthLeases, CustomerListBoat } from './types';
 
-export const getCustomersData = (data: CUSTOMERS | undefined): CustomerData[] => {
-  return (
-    data?.profiles?.edges.reduce<CustomerData[]>((acc, profileEdge) => {
-      if (profileEdge?.node) {
-        const profile = profileEdge.node;
-        const boats = getBoats(profile) ?? [];
-        const applications = getApplications(profile) ?? [];
-        const berthLeases = getBerthLeases(profile) ?? [];
-
-        const profileData: CustomerData = {
-          address: profile.primaryAddress?.address,
-          city: profile.primaryAddress?.city,
-          email: profile.primaryEmail?.email,
-          id: profile.id,
-          name: `${profile.lastName} ${profile.firstName}`,
-          organization: profile.organization,
-          phone: profile.primaryPhone?.phone ?? undefined,
-          postalCode: profile.primaryAddress?.postalCode,
-          comment: profile.comment,
-          berthsColumnData: '',
-          billsColumnData: '',
-          boatsColumnData: '',
-          boats,
-          applications,
-          berthLeases,
-          customerGroup: profile.customerGroup,
-        };
-        return [...acc, profileData];
-      }
-      return acc;
-    }, []) ?? []
-  );
-};
-
 const getBoats = (profile: ProfileNode): CustomerListBoat[] | undefined => {
   return profile.boats?.edges
     .filter((edge) => edge && edge.node)
@@ -75,3 +41,39 @@ function getBerthLeases(profile: ProfileNode): CustomerListBerthLeases[] | undef
       };
     });
 }
+
+export const getCustomersData = (data: CUSTOMERS | undefined): CustomerData[] => {
+  return (
+    data?.profiles?.edges.reduce<CustomerData[]>((acc, profileEdge) => {
+      if (profileEdge?.node) {
+        const profile = profileEdge.node;
+        const boats = getBoats(profile) ?? [];
+        const applications = getApplications(profile) ?? [];
+        const berthLeases = getBerthLeases(profile) ?? [];
+
+        const { organization, primaryAddress } = profile;
+
+        const profileData: CustomerData = {
+          address: organization?.address ?? primaryAddress?.address,
+          city: organization?.city ?? primaryAddress?.city,
+          email: profile.primaryEmail?.email,
+          id: profile.id,
+          name: `${profile.lastName} ${profile.firstName}`,
+          organization: organization,
+          phone: profile.primaryPhone?.phone ?? undefined,
+          postalCode: organization?.postalCode ?? primaryAddress?.postalCode,
+          comment: profile.comment,
+          berthsColumnData: '',
+          billsColumnData: '',
+          boatsColumnData: '',
+          boats,
+          applications,
+          berthLeases,
+          customerGroup: profile.customerGroup,
+        };
+        return [...acc, profileData];
+      }
+      return acc;
+    }, []) ?? []
+  );
+};
