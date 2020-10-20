6120d248c843dfcc5a3ccc1853fc4e71dd265e1c
Tatu Arvela <tatu.arvela@nitor.com>
2020-10-20 13:34:58 +0300

    Adjust (berth) applicationList utils to match other application utils

--- src/features/applicationList/utils.ts
+++ src/features/applicationList/utils.ts
@@ -1,4 +1,8 @@
-import { BERTH_APPLICATIONS } from './__generated__/BERTH_APPLICATIONS';
+import {
+  BERTH_APPLICATIONS,
+  BERTH_APPLICATIONS_berthApplications_edges as BERTH_APPLICATIONS_EDGE,
+  BERTH_APPLICATIONS_berthApplications_edges_node as BERTH_APPLICATIONS_NODE,
+} from './__generated__/BERTH_APPLICATIONS';
 import { ApplicationStatus, LeaseStatus } from '../../@types/__generated__/globalTypes';
 
 interface HarborChoice {
@@ -25,112 +29,110 @@ interface BerthSwitch {
 }
 
 export interface ApplicationData {
-  id: string;
-  firstName: string;
-  lastName: string;
-  isSwitch: boolean;
-  customerId?: string;
-  berthSwitch: BerthSwitch | null;
-  queue: number;
-  createdAt: string;
-  municipality: string;
-  status: ApplicationStatus;
-  lease: Lease | null;
-  boatType?: string | null;
-  boatRegistrationNumber: string;
-  boatWidth: number;
-  boatLength: number;
-  boatDraught: number | null;
-  boatWeight: number | null;
-  boatName: string;
-  boatModel: string;
-  choices: Array<HarborChoice>;
   accessibilityRequired: boolean;
+  berthSwitch: BerthSwitch | null;
+  boatDraught: number | null;
+  boatLength: number;
+  boatModel: string;
+  boatName: string;
+  boatRegistrationNumber: string;
+  boatType?: string | null;
+  boatWeight: number | null;
+  boatWidth: number;
+  choices: Array<HarborChoice>;
+  createdAt: string;
+  customerId?: string;
+  firstName: string;
+  id: string;
+  isSwitch: boolean;
+  lastName: string;
+  lease: Lease | null;
+  municipality: string;
+  queue: number;
+  status: ApplicationStatus;
 }
 
 export const getBerthApplicationData = (data: BERTH_APPLICATIONS | undefined): ApplicationData[] => {
   const boatTypes = data?.boatTypes;
 
   return (
-    data?.berthApplications?.edges.reduce<ApplicationData[]>((acc, application) => {
-      if (application?.node) {
-        const {
-          id,
-          firstName,
-          lastName,
-          customer,
-          berthSwitch,
-          createdAt,
-          municipality,
-          status,
-          lease,
-          boatDraught,
-          boatRegistrationNumber,
-          boatModel,
-          boatName,
-          boatWidth,
-          boatLength,
-          boatType,
-          boatWeight,
-          accessibilityRequired,
-        } = application.node;
+    data?.berthApplications?.edges.reduce<ApplicationData[]>((acc, edge) => {
+      const {
+        accessibilityRequired,
+        berthSwitch,
+        boatDraught,
+        boatLength,
+        boatModel,
+        boatName,
+        boatRegistrationNumber,
+        boatType,
+        boatWeight,
+        boatWidth,
+        createdAt,
+        customer,
+        firstName,
+        harborChoices,
+        id,
+        lastName,
+        lease,
+        municipality,
+        status,
+      } = (edge as BERTH_APPLICATIONS_EDGE).node as BERTH_APPLICATIONS_NODE;
 
-        const choices =
-          application.node.harborChoices?.map((choice) => {
-            return {
-              priority: choice?.priority ?? Number.MAX_VALUE,
-              harbor: choice?.harbor ?? '',
-              harborName: choice?.harborName ?? '',
-            };
-          }) ?? [];
-
-        let leaseProps: Lease | null = null;
-        if (lease?.berth?.pier.properties?.harbor) {
-          leaseProps = {
-            berthNum: lease.berth.number || '',
-            harborId: lease.berth.pier.properties.harbor.id,
-            harborName: lease.berth.pier.properties.harbor.properties?.name || '',
-            id: lease.id,
-            pierIdentifier: lease.berth.pier.properties?.identifier || '',
-            status: lease.status,
+      const choices =
+        harborChoices?.map((choice) => {
+          return {
+            priority: choice?.priority ?? Number.MAX_VALUE,
+            harbor: choice?.harbor ?? '',
+            harborName: choice?.harborName ?? '',
           };
-        }
+        }) ?? [];
 
-        const berthSwitchProps = berthSwitch && {
-          berthNum: berthSwitch.berthNumber,
-          harborId: berthSwitch.harbor,
-          harborName: berthSwitch.harborName,
-          pierIdentifier: berthSwitch.pier,
-          reason: berthSwitch.reason?.title || null,
+      let leaseProps: Lease | null = null;
+      if (lease?.berth?.pier.properties?.harbor) {
+        leaseProps = {
+          berthNum: lease.berth.number || '',
+          harborId: lease.berth.pier.properties.harbor.id,
+          harborName: lease.berth.pier.properties.harbor.properties?.name || '',
+          id: lease.id,
+          pierIdentifier: lease.berth.pier.properties?.identifier || '',
+          status: lease.status,
         };
-
-        const applicationData = {
-          id,
-          firstName,
-          lastName,
-          customerId: customer?.id,
-          isSwitch: !!berthSwitch,
-          berthSwitch: berthSwitchProps,
-          queue: 0, // TODO
-          createdAt,
-          municipality,
-          status,
-          lease: leaseProps,
-          boatRegistrationNumber,
-          boatModel,
-          boatName,
-          boatWidth,
-          boatLength,
-          boatDraught,
-          boatWeight,
-          boatType: boatTypes?.find(({ id }) => id === boatType)?.name,
-          choices,
-          accessibilityRequired,
-        };
-
-        return [...acc, applicationData];
       }
-      return acc;
+
+      const berthSwitchProps = berthSwitch && {
+        berthNum: berthSwitch.berthNumber,
+        harborId: berthSwitch.harbor,
+        harborName: berthSwitch.harborName,
+        pierIdentifier: berthSwitch.pier,
+        reason: berthSwitch.reason?.title || null,
+      };
+
+      const applicationData = {
+        accessibilityRequired,
+        berthSwitch: berthSwitchProps,
+        boatDraught,
+        boatLength,
+        boatModel,
+        boatName,
+        boatRegistrationNumber,
+        boatType: boatTypes?.find(({ id }) => id === boatType)?.name,
+        boatWeight,
+        boatWidth,
+        choices,
+        createdAt,
+        customerId: customer?.id,
+        firstName,
+        id,
+        isSwitch: !!berthSwitch,
+        lastName,
+        lease: leaseProps,
+        municipality,
+        queue: 0, // TODO
+        status,
+      };
+
+      return [...acc, applicationData];
     }, []) ?? []
   );
 };
