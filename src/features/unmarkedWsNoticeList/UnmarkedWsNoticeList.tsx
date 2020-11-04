a08f0aa7ab2de1144c0ab281292f45162084295a
Tatu Arvela <tatu.arvela@nitor.com>
2020-11-04 10:54:56 +0200

    Add ApplicationListTools to unmarked WS notices

--- src/features/unmarkedWsNoticeList/UnmarkedWsNoticeList.tsx
+++ src/features/unmarkedWsNoticeList/UnmarkedWsNoticeList.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { SortingRule } from 'react-table';
+import { toast } from 'react-toastify';
 
 import PageTitle from '../../common/pageTitle/PageTitle';
 import PageContent from '../../common/pageContent/PageContent';
@@ -10,23 +11,31 @@ import { formatDate } from '../../common/utils/format';
 import StatusLabel from '../../common/statusLabel/StatusLabel';
 import { APPLICATION_STATUS } from '../../common/utils/constants';
 import { ApplicationStatus } from '../../@types/__generated__/globalTypes';
-import { UnmarkedWinterStorageNotice } from './utils';
+import { getDraftedOffers, UnmarkedWinterStorageNotice } from './utils';
 import UnmarkedWsNoticeDetails from '../unmarkedWsNoticeDetails/UnmarkedWsNoticeDetails';
 import Pagination from '../../common/pagination/Pagination';
 import Grid from '../../common/grid/Grid';
 import ApplicationStateTableTools from '../../common/tableTools/applicationStateTableTools/ApplicationStateTableTools';
+import ApplicationListTools from '../applicationListTools/ApplicationListTools';
+import hdsToast from '../../common/toast/hdsToast';
 
+interface Order {
+  orderId: string;
+  email: string;
+}
 export interface UnmarkedWsNoticeListProps {
-  notices: UnmarkedWinterStorageNotice[];
+  count?: number;
+  isSubmittingApproveOrders: boolean;
   loading: boolean;
+  notices: UnmarkedWinterStorageNotice[];
   pageCount: number;
   pageIndex: number;
   sortBy: SortingRule<UnmarkedWinterStorageNotice>[];
-  count?: number;
   statusFilter?: ApplicationStatus;
-  onStatusFilterChange(statusFilter?: ApplicationStatus): void;
-  onSortedColsChange(sortedCol: SortingRule<UnmarkedWinterStorageNotice>[]): void;
   goToPage(page: number): void;
+  handleApproveOrders(orders: Order[]): Promise<void>;
+  onSortedColsChange(sortedCol: SortingRule<UnmarkedWinterStorageNotice>[]): void;
+  onStatusFilterChange(statusFilter?: ApplicationStatus): void;
 }
 
 type ColumnType = Column<UnmarkedWinterStorageNotice>;
@@ -36,12 +45,14 @@ const UnmarkedWsNoticeList = ({
   loading,
   pageCount,
   pageIndex,
+  isSubmittingApproveOrders,
   goToPage,
   count,
   statusFilter,
   onStatusFilterChange,
   onSortedColsChange,
   sortBy,
+  handleApproveOrders,
 }: UnmarkedWsNoticeListProps) => {
   const { t, i18n } = useTranslation();
   const columns: ColumnType[] = [
@@ -108,16 +119,42 @@ const UnmarkedWsNoticeList = ({
             <UnmarkedWsNoticeDetails {...row.original} />
           </Grid>
         )}
+        renderTableToolsTop={({ selectedRows }, { resetSelectedRows }) => {
+          const offers = getDraftedOffers(selectedRows);
+          const offersWithoutPlacesCount = selectedRows.filter((row) => !row.leaseId).length;
+
+          let toastId;
+          if (offersWithoutPlacesCount > 0)
+            toastId = hdsToast({
+              autoDismiss: false,
+              type: 'error',
+              toastId: 'multiUnMarkedWSApplicationsError',
+              labelText: 'applicationList.errors.unhandledApplications.label',
+              text: 'applicationList.errors.unhandledApplications.description',
+              translated: true,
+            });
+          else toast.dismiss(toastId);
+
+          return (
+            <>
+              <ApplicationListTools
+                offersCount={offers.length}
+                selectedApplicationsCount={selectedRows.length}
+                isSubmitting={isSubmittingApproveOrders}
+                clearSelectedRows={resetSelectedRows}
+                handleApproveOrders={() => handleApproveOrders(offers)}
+              />
+              <ApplicationStateTableTools
+                count={count}
+                statusFilter={statusFilter}
+                onStatusFilterChange={onStatusFilterChange}
+              />
+            </>
+          );
+        }}
         renderMainHeader={() => t('unmarkedWsNotices.list.title')}
         renderTableToolsBottom={() => (
           <Pagination forcePage={pageIndex} pageCount={pageCount} onPageChange={({ selected }) => goToPage(selected)} />
-        )}
-        renderTableToolsTop={() => (
-          <ApplicationStateTableTools
-            count={count}
-            statusFilter={statusFilter}
-            onStatusFilterChange={onStatusFilterChange}
-          />
         )}
         renderEmptyStateRow={() => t('common.notification.noData.description')}
         onSortedColsChange={onSortedColsChange}
