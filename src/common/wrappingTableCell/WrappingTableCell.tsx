bfa49a3c14f612d45f493084f74f6d90ee53b4ed
Tatu Arvela <tatu.arvela@nitor.com>
2020-11-16 16:31:16 +0200

    Add component for wrapping table cell content

---
+++ src/common/wrappingTableCell/WrappingTableCell.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './wrappingTableCell.module.scss';
+
+export interface WrappingTableCellProps {
+  children: React.ReactNode;
+}
+
+const WrappingTableCell = ({ children }: WrappingTableCellProps) => (
+  <p className={styles.wrappingTableCell}>{children}</p>
+);
+
+export default WrappingTableCell;
