5caee10edd236bdb8edb2ff20fab6927e5dc33b5
Tatu Arvela <tatu.arvela@nitor.com>
2020-11-17 12:40:25 +0200

    Show horizontal rule if there are previous (inactive) berth leases

--- src/features/customerList/customerDetails/__tests__/CustomerDetails.test.tsx
+++ src/features/customerList/customerDetails/__tests__/CustomerDetails.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { mount } from 'enzyme';
 
-import CustomerDetails from '../CustomerDetails';
+import CustomerDetails, { CustomerDetailsProps } from '../CustomerDetails';
 import {
   customerListApplications,
   customerListBerthLeases,
@@ -10,30 +10,49 @@ import {
   customerListEntry,
   customerListWinterStoragePlaces,
 } from '../__mocks__/mockData';
+import { CustomerListBerthLeases } from '../../types';
+
+const mockProps = {
+  ...customerListEntry,
+  berths: customerListBerthLeases,
+  winterStoragePlaces: customerListWinterStoragePlaces,
+  boats: customerListBoats,
+  applications: customerListApplications,
+  invoices: customerListInvoices,
+};
 
 describe('CustomerDetails', () => {
-  const getWrapper = () =>
-    mount(
-      <CustomerDetails
-        name={customerListEntry.name}
-        address={customerListEntry.address}
-        postalCode={customerListEntry.postalCode}
-        city={customerListEntry.city}
-        phone={customerListEntry.phone}
-        email={customerListEntry.email}
-        berths={customerListBerthLeases}
-        winterStoragePlaces={customerListWinterStoragePlaces}
-        boats={customerListBoats}
-        applications={customerListApplications}
-        invoices={customerListInvoices}
-        comment={customerListEntry.comment}
-        customerGroup={customerListEntry.customerGroup}
-      />
-    );
+  const getWrapper = (props?: Partial<CustomerDetailsProps>) => mount(<CustomerDetails {...mockProps} {...props} />);
 
   it('renders normally', () => {
     const wrapper = getWrapper();
 
     expect(wrapper.render()).toMatchSnapshot();
+  });
+
+  it('renders a horizontal rule if there are inactive berth leases', () => {
+    const berths: CustomerListBerthLeases[] = [
+      { id: '123', isActive: false, title: 'Pursilahdenranta B31' },
+      { id: '321', isActive: true, title: 'Strömsinlahdenranta B31' },
+    ];
+
+    const wrapper = getWrapper({
+      berths,
+    });
+
+    expect(wrapper.find('hr').exists()).toEqual(true);
+  });
+
+  it('renders no horizontal rule if there are no inactive berth leases', () => {
+    const berths: CustomerListBerthLeases[] = [
+      { id: '123', isActive: true, title: 'Pursilahdenranta B31' },
+      { id: '321', isActive: true, title: 'Strömsinlahdenranta B31' },
+    ];
+
+    const wrapper = getWrapper({
+      berths,
+    });
+
+    expect(wrapper.find('hr').exists()).toEqual(false);
   });
 });
