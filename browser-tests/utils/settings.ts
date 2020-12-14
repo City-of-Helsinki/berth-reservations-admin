4a3ead9354cb680d8c7feefadada2a4bc733828c
Nizar Rahme <nizar.w.rahmah@gmail.com>
2020-12-14 15:14:08 +0200

    Add local url to the environment vars

--- browser-tests/utils/settings.ts
+++ browser-tests/utils/settings.ts
@@ -1,8 +1,6 @@
 // @ts-ignore
 require('dotenv').config({ path: '.env.development.local' });
 
-const LOCAL_ENV_URL = 'http://localhost:3000';
-
 export const testUsername = (): string => {
   if (!process.env.BROWSER_TESTS_UID) {
     throw new Error('No BROWSER_TESTS_UID specified.');
@@ -17,4 +15,4 @@ export const testUserPassword = (): string => {
   return process.env.BROWSER_TESTS_PWD;
 };
 
-export const envUrl = (): string => LOCAL_ENV_URL;
+export const envUrl = (): string => process.env.REACT_APP_LOCAL_ENV_URL;
