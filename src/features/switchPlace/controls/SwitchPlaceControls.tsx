6313acb5cb9a30ff32d30ab404c84f2dc51b6f13
Tatu Arvela <tatu.arvela@nitor.com>
2021-04-12 15:14:12 +0300

    Merge SwitchPlaceButton with SwitchPlaceControls

--- src/features/switchPlace/controls/SwitchPlaceControls.tsx
+++ src/features/switchPlace/controls/SwitchPlaceControls.tsx
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
 
 import SwitchPlaceModal from './SwitchPlaceModal';
 import { LeaseStatus } from '../../../@types/__generated__/globalTypes';
-import SwitchPlaceButton from './SwitchPlaceButton';
 import { Lease } from '../../customerView/leasesCard/types';
+import Button from '../../../common/button/Button';
 
 export type SwitchPlaceProps = Pick<Lease, 'id' | 'type' | 'status'>;
 
 const SwitchPlaceControls = ({ id, type, status }: SwitchPlaceProps) => {
+  const { t } = useTranslation();
   const [modalVisible, setModalVisible] = useState<boolean>(false);
 
   const canPlaceBeSwitched = (status: LeaseStatus) => status === LeaseStatus.PAID;
@@ -15,7 +17,11 @@ const SwitchPlaceControls = ({ id, type, status }: SwitchPlaceProps) => {
   if (type === 'winterStorage') return null; // Not implemented for winter storage
   return (
     <>
-      {canPlaceBeSwitched(status) && <SwitchPlaceButton onClick={() => setModalVisible(true)} />}
+      {canPlaceBeSwitched(status) && (
+        <Button onClick={() => setModalVisible(true)} size="small">
+          {t('customerView.leases.switchPlace.switchBerth')}
+        </Button>
+      )}
       {modalVisible && <SwitchPlaceModal closeModal={() => setModalVisible(false)} leaseId={id} />}
     </>
   );
