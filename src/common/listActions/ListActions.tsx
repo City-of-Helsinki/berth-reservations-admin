29b1d63594171677ecad5731b1410e77e76e636e
Nizar Rahme <nizar.w.rahmah@gmail.com>
2021-04-28 11:44:06 +0300

    Allow disabling submit button & custome className

--- src/common/listActions/ListActions.tsx
+++ src/common/listActions/ListActions.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import classNames from 'classnames';
 import { useTranslation } from 'react-i18next';
 
 import Button from '../button/Button';
@@ -7,26 +8,29 @@ import Select from '../select/Select';
 import styles from './listActions.module.scss';
 
 type ListActionItem<T> = {
-  id: string;
+  id: string | number;
   label: string;
   buttonText?: string;
+  buttonDisabled?: boolean;
   onClick?(selectedRows: T[]): void;
-  renderComponent?(): React.ReactNode;
+  renderComponent?(deselect: () => void): React.ReactNode;
 };
 
 interface ListActionsProps<T> {
+  className?: string;
   selectedRows: T[];
   listActions: ListActionItem<T>[];
   resetSelectedRows(): void;
 }
 
 const ListActions = <T extends object | string | number | boolean | bigint | symbol>({
+  className,
   selectedRows,
   resetSelectedRows,
   listActions,
 }: ListActionsProps<T>) => {
   const { t } = useTranslation();
-  const [selectedActionId, setSelectedActionId] = useState<string>();
+  const [selectedActionId, setSelectedActionId] = useState<string | number>();
   const hasSelectedRows = !!selectedRows.length;
 
   const options = [
@@ -39,20 +43,20 @@ const ListActions = <T extends object | string | number | boolean | bigint | sym
   const selectedAction = listActions.find((action) => action.id === selectedActionId);
 
   return (
-    <div className={styles.container}>
+    <div className={classNames(styles.container, className)}>
       <Select
         onChange={(e) => setSelectedActionId(listActions.find((action) => action.id === e.target.value)?.id)}
         value={selectedActionId ?? ''}
         options={options}
         className={styles.select}
       />
-      {selectedAction?.renderComponent?.()}
+      {selectedAction?.renderComponent?.(() => setSelectedActionId(undefined))}
       {selectedAction?.onClick && (
         <div className={styles.buttonContainer}>
           <Button
             onClick={() => selectedAction.onClick?.(selectedRows)}
             variant="secondary"
-            disabled={!hasSelectedRows}
+            disabled={!hasSelectedRows || selectedAction.buttonDisabled}
           >
             {!hasSelectedRows ? t('buttonWithSelectedRows.noSelection') : selectedAction.buttonText}
           </Button>
