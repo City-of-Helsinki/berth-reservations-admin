e8b8cd023888da37cbd6c9a727b9016957d6d328
Tatu Arvela <tatu.arvela@nitor.com>
2021-05-10 11:53:12 +0300

    Make browser tests endure long loading times

--- browser-tests/navigateAndView.ts
+++ browser-tests/navigateAndView.ts
@@ -10,48 +10,60 @@ import { winterStorageAreas } from './pages/winterStorageAreas';
 
 fixture('Navigate and view').page(envUrl());
 
-test('Navigate and view basic data', async (t) => {
+test('Berth applications', async (t) => {
   await login(t);
-
-  // Applications
+  await t.click(navigation.applicationsExpandable).click(navigation.applications);
   await t
-    .click(navigation.applicationsExpandable)
-    .click(navigation.applications)
     .expect(applications.applicationList.firstApplicationLink.exists)
     .ok()
     .click(applications.applicationList.firstApplicationLink)
     .expect(applications.applicationView.firstName.filter(hasLength).exists)
     .ok();
+});
 
-  // Winter storage applications
+test('Winter storage applications', async (t) => {
+  await login(t);
+  await t.click(navigation.winterStorageApplications);
   await t
-    .click(navigation.winterStorageApplications)
+    .expect(applications.applicationList.firstApplicationLink.exists)
+    .ok()
     .click(applications.applicationList.firstApplicationLink)
     .expect(applications.applicationView.firstName.filter(hasLength).exists)
     .ok();
+});
 
-  // Harbors
+test('Harbors', async (t) => {
+  await login(t);
+  await t.click(navigation.harbors);
   await t
-    .click(navigation.harbors)
+    .expect(harbors.harborList.firstHarborLink.exists)
+    .ok()
     .click(harbors.harborList.firstHarborLink)
     .expect(harbors.harborView.address.filter(hasLength).exists)
     .ok();
+});
 
-  // Winter storage areas
-  await t
-    .click(navigation.winterStorageAreas)
-    .expect(winterStorageAreas.winterStorageAreaList.firstWinterStorageArea.filter(hasLength).exists)
-    .ok();
+test('Winter storage areas', async (t) => {
+  await login(t);
+  await t.click(navigation.winterStorageAreas);
+  await t.expect(winterStorageAreas.winterStorageAreaList.firstWinterStorageArea.filter(hasLength).exists).ok();
+});
 
-  // Customers
+test('Customers', async (t) => {
+  await login(t);
+  await t.click(navigation.customers);
   await t
-    .click(navigation.customers)
+    .expect(customers.customerList.firstCustomerLink.exists)
+    .ok()
     .click(customers.customerList.firstCustomerLink)
     .expect(customers.customerView.firstDataLabel.filter(hasLength).exists)
     .ok();
+});
 
-  // Pricing
-  await t.click(navigation.pricing).expect(pricing.berthPrices.tier1Price.filter(hasPrice).exists).ok();
+test('Pricing', async (t) => {
+  await login(t);
+  await t.click(navigation.pricing);
+  await t.expect(pricing.berthPrices.tier1Price.filter(hasPrice).exists).ok();
 
   // Pricing modal
   await t
