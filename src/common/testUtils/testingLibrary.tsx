5266150e345c34cd2ac022904c246e7a499d42a2
Jaakko Jokinen <jaakko.jokinen@teamit.fi>
2022-02-14 14:39:38 +0200

    Fix test utils types

--- src/common/testUtils/testingLibrary.tsx
+++ src/common/testUtils/testingLibrary.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, RenderOptions } from '@testing-library/react';
 import { I18nextProvider } from 'react-i18next';
 import userEvent from '@testing-library/user-event';
-import { MockedProvider } from '@apollo/react-testing';
+import { MockedProvider, MockedResponse } from '@apollo/react-testing';
 import { HashRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 
 import i18n from '../../locales/i18n';
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-// @ts-ignore
-const AllTheProviders = ({ children, mocks }) => {
+type AllTheProvidersProps = {
+  children: JSX.Element;
+  mocks?: MockedResponse[];
+};
+
+const AllTheProviders = ({ children, mocks }: AllTheProvidersProps) => {
   return (
     <RecoilRoot>
       <I18nextProvider i18n={i18n}>
@@ -22,9 +25,11 @@ const AllTheProviders = ({ children, mocks }) => {
   );
 };
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-// @ts-ignore
-const customRender = (ui, { mocks, ...options }) =>
+type ExtendedRenderOptions = RenderOptions & {
+  mocks?: MockedResponse[];
+};
+
+const customRender = (ui: React.ReactElement, { mocks, ...options }: ExtendedRenderOptions) =>
   render(ui, { wrapper: (props) => <AllTheProviders {...props} mocks={mocks} />, ...options });
 
 // re-export everything
