c46a7d3149d3dc45c4324366d089b04002cfb25e
Jaakko Jokinen <jaakko.jokinen@teamit.fi>
2022-02-17 10:23:54 +0200

    Remember search over page navigations

--- src/features/customerList/customerListTableFilters/CustomerListTableFilters.tsx
+++ src/features/customerList/customerListTableFilters/CustomerListTableFilters.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { IconAngleDown } from 'hds-react';
 import { useTranslation } from 'react-i18next';
+import { useHistory, useLocation } from 'react-router-dom';
 
 import useListTableFilters from './useListTableFilters';
 import CustomerListTableFiltersFormContainer from './CustomerListTableFiltersFormContainer';
@@ -14,6 +15,7 @@ enum Content {
 }
 
 const CustomerListTableFilters = () => {
+  usePersistedSearch();
   const { t } = useTranslation();
   const [areFiltersExpanded, setFiltersExpanded] = React.useState<boolean>(false);
   const [listTableFilters] = useListTableFilters();
@@ -51,6 +53,27 @@ function getVariant(areFiltersExpanded: boolean, someFilterIsActive: boolean) {
   }
 
   return Content.EMPTY;
+}
+
+const PERSISTED_SEARCH_SESSION_STORAGE_KEY = 'berth-reservations-admin/persistedCustomerListTableFilters';
+
+function usePersistedSearch() {
+  const { search, pathname } = useLocation();
+  const history = useHistory();
+  const persistedSearch = sessionStorage.getItem(PERSISTED_SEARCH_SESSION_STORAGE_KEY);
+
+  useEffect(() => {
+    // Whenever search changes, persist it for the duration of the session
+    sessionStorage.setItem(PERSISTED_SEARCH_SESSION_STORAGE_KEY, search);
+  }, [search]);
+
+  useEffect(() => {
+    // If search is empty and we have a persisted search, apply the persisted
+    // search
+    if (!search && persistedSearch) {
+      history.replace(`${pathname}${persistedSearch}`);
+    }
+  }, [history, pathname, persistedSearch, search]);
 }
 
 export default CustomerListTableFilters;
